874a5f5c6584ab07de2afe2a1ad7d7435fdc515a
Iheb Ben Naceur <129274519+ihebbennaceur@users.noreply.github.com>
2025-03-08 17:11:10 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-const mongoose=require("mongoose");
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin', 'agent'], default: 'user' }
+});
 
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
-const userSchema=new mongoose.Schema(
-
-
-    {
-        name:String,
-        lastname:String,
-     email:{required:true,unique:true,type:String}  ,
-     password:String,
-    }
-)
-
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', UserSchema);
