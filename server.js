eac616b1f31501d0e1545df6925a86a4d76ab4f7
ihebbennaceur <ihebbennaceur6@gmail.com>
2025-03-10 19:15:04 +0100

    imprting packages

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/dbconnection.js");
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan"); // Add Morgan
 
-const app=express();
+const authRoutes = require("./routes/authRoutes");
+const ticketRoutes = require("./routes/ticketRoutes");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
 
 connectDB();
-
 
 
 const PORT = process.env.PORT || 9092;
